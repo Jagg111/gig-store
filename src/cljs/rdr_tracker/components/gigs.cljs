d8426683a57065d8ef0aa603d74cd7915bf11284
Mitchel Burton <jaggdev@gmail.com>
2018-12-20 09:11:06 -0600

    Gigs via atom
    Added gigs with an atom. Pulled that in and baked out the gigs component to display them.

--- src/cljs/rdr_tracker/components/gigs.cljs
+++ src/cljs/rdr_tracker/components/gigs.cljs
@@ -1,4 +1,15 @@
-(ns rdr-tracker.components.gigs)
+(ns rdr-tracker.components.gigs
+  (:require [rdr-tracker.state :as gigstate]))
 
 (defn gigs []
-  [:gigs "gigs"])
+  [:main
+   [:div.gigs
+    (for [{:keys [id img title price desc]} (vals @gigstate/gigs)]
+      [:div.gig {:key id}
+       [:img.gig__artwork {:src img :alt title}]
+       [:div.gig__body
+        [:div.gig__title
+         [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Blah"}
+          [:i.icon.icon--plus]] title]
+        [:p.gig__price price]
+        [:p.gig__desc desc]]])]])
