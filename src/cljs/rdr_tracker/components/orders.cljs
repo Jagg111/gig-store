01cff280989ce4a39cb906bad0f80eb2206c49f5
Mitchel Burton <jaggdev@gmail.com>
2018-12-20 11:17:52 -0600

    Add component to show orders
    New order component that pulls state of out the atom and shows the orders.

--- src/cljs/rdr_tracker/components/orders.cljs
+++ src/cljs/rdr_tracker/components/orders.cljs
@@ -1,4 +1,15 @@
-(ns rdr-tracker.components.orders)
+(ns rdr-tracker.components.orders
+  (:require [rdr-tracker.state :as state]))
 
 (defn orders []
-  [:orders "orders"])
+  [:aside
+   [:div.order
+    [:div.gig__body (for [[id quant] @state/orders]
+                      [:dev.item {:key id}
+                       [:div.img
+                        [:img {:src (get-in @state/gigs [id :img])
+                               :alt (get-in @state/gigs [id :title])}]]
+                       [:div.content
+                        [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
+                       [:div.action
+                        [:div.price (* (get-in @state/gigs [id :price]) quant)]]])]]])
